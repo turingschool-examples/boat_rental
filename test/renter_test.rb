98a1db0e8f0bb3dc1167943c13839fc54f069b1a
Ben Lee <bendelonlee@gmail.com>
2018-10-24 09:22:27 -0600

    test_it_exists passes for Renter

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,4 +3,10 @@ require './lib/renter'
 
 
 class RenterTest < Minitest::Test
+  def setup
+    @renter = Renter.new()
+  end
+  def test_it_exists
+    assert_instance_of Renter, @renter
+  end
 end
