0e1397fc148546d16e579c918133f255e59558da
vjp888 <rellik888@gmail.com>
2018-10-24 09:09:19 -0600

    Added reader to boat and tested

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,6 +1,6 @@
 
 class Boat
-
+  attr_reader :type, :price_per_hour
   def initialize(type, price_per_hour)
     @type = type
     @price_per_hour = price_per_hour
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -6,7 +6,19 @@ class BoatTest < MiniTest::Test
 
   def test_boat_exists
     kayak = Boat.new(:kayak, 20)
-    
+
     assert_instance_of Boat, kayak
+  end
+
+  def test_it_can_give_type
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal :kayak, kayak.type
+  end
+
+  def test_it_can_giv_pph
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 20, kayak.price_per_hour
   end
 end
