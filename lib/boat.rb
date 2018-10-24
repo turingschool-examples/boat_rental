032e7eb331ba515c257f549700bc3bf544900aff
Leah Miller <lkmiller.87@gmail.com>
2018-10-24 09:06:49 -0600

    Add passing test for type

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,4 +1,6 @@
 class Boat
+
+  attr_reader :type
   def initialize(type, price_per_hour)
     @type = type
     @price_per_hour = price_per_hour
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -5,6 +5,11 @@ require './lib/boat'
 class BoatTest < Minitest::Test
   def test_it_exists
     kayak = Boat.new(:kayak, 20)
-    assert_instance_of Boat, kayak 
+    assert_instance_of Boat, kayak
+  end
+
+  def test_it_has_a_type
+    kayak = Boat.new(:kayak, 20)
+    assert_equal :kayak, kayak.type 
   end
 end
