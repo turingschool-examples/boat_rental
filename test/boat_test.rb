67193cd5cbf3048757aefdd565a3255e2f2076e3
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-24 09:13:03 -0600

    Adds remaining tests to finish iteration 1

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -8,4 +8,26 @@ class BoatTest < Minitest::Test
     kayak = Boat.new(:kayak, 20)
     assert_instance_of Boat, kayak
   end
+  def test_a_boat_has_a_type
+    kayak = Boat.new(:kayak, 20)
+    assert_equal :kayak, kayak.type
+  end
+
+  def test_a_boat_has_a_price_per_hour
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 20, kayak.price_per_hour
+  end
+
+  def test_how_many_hours_a_boat_has_been_rented
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_you_can_add_hours_rented_to_the_boat
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+    assert_equal 3, kayak.hours_rented
+  end
 end
