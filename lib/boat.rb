76ca319f43adbf42c1554b0c5605942d491548ac
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:14:29 -0600

    adds code for passing fourth test

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,7 +1,11 @@
 class Boat
-  attr_reader :type 
+  attr_reader :type,
+              :price_per_hr,
+              :hours_rented
   def initialize(type, price_per_hr)
     @type = type
+    @price_per_hr = price_per_hr
+    @hours_rented = 0
   end
 
 end
