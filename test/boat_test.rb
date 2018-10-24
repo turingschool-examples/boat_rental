3857d8b071ca8c1188991a50827c0ab01c88ed4b
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-24 09:09:30 -0600

    Add boat class functionality

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -10,31 +10,26 @@ class BoatTest < Minitest::Test
     end
     
     def test_it_has_a_type
-      skip
       kayak = Boat.new(:kayak, 20)
       assert_equal :kayak, kayak.type
     end
     
     def test_it_has_a_price_per_hour
-      skip
       kayak = Boat.new(:kayak, 20)
       assert_equal 20, kayak.price_per_hour
     end
     
     def test_hours_rented_starts_as_zero
-      skip
       kayak = Boat.new(:kayak, 20)
       assert_equal 0, kayak.hours_rented
     end
     
     def test_it_can_add_hours_rented
-      skip
       kayak = Boat.new(:kayak, 20)
       assert_equal 1, kayak.add_hour
       kayak.add_hour
       kayak.add_hour
       assert_equal 3, kayak.hours_rented
     end
-    
   
 end
