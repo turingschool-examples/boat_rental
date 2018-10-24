27ac2423a5b48070d6eb040f211a2d1185390b60
Leah Miller <lkmiller.87@gmail.com>
2018-10-24 09:05:13 -0600

    Add passing test for initialize

---
+++ lib/boat.rb
@@ -0,0 +1,6 @@
+class Boat
+  def initialize(type, price_per_hour)
+    @type = type
+    @price_per_hour = price_per_hour
+  end
+end
---
+++ test/boat_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/boat'
+
+class BoatTest < Minitest::Test
+  def test_it_exists
+    kayak = Boat.new(:kayak, 20)
+    assert_instance_of Boat, kayak 
+  end
+end
