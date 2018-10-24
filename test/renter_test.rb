822df3acb6266b32851cc93a818ddc9fdb8366c1
Ben Lee <bendelonlee@gmail.com>
2018-10-24 09:26:01 -0600

    Adds init tests for renter

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -4,9 +4,15 @@ require './lib/renter'
 
 class RenterTest < Minitest::Test
   def setup
-    @renter = Renter.new()
+    @renter = Renter.new("Patrick Star", "4242424242424242")
   end
   def test_it_exists
     assert_instance_of Renter, @renter
+  end
+  def test_it_can_be_initialized_with_name
+    assert_equal "Patrick Star", @renter.name
+  end
+  def test_it_can_be_initialized_with_credit_card_number
+    assert_equal "4242424242424242", @renter.credit_card_number
   end
 end
