b977155ed02339df932eef9490f99e6bb5643b21
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:28:06 -0600

    adds first test for dock

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -2,3 +2,11 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
 require 'pry'
+
+class DockTest < Minitest::Test
+  def test_it_exists
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_instance_of Dock, dock
+  end
+
+end
