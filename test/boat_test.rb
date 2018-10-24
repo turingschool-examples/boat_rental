c2675ed845ee8ed8c5aa0c06579fc9d3211db363
Tony Robertson <tonyr729@gmail.com>
2018-10-24 09:15:25 -0600

    Add tests for hours_rented property

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -18,4 +18,8 @@ class MerchantTest < Minitest::Test
   def test_it_has_a_price_per_hour
     assert_equal 20, @kayak.price_per_hour
   end
+
+  def test_it_starts_at_0_hours_rented
+    assert_equal 0, @kayak.hours_rented
+  end
 end
