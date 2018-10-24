4ad1c5e9d5d9d1f0925bb4b3610e32a250d9b815
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:13:51 -0600

    adds third passing test and new fourth test

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -17,6 +17,11 @@ class BoatTest < Minitest::Test
   def test_it_has_a_price_per_hr
     kayak = Boat.new(:kayak, 20)
     assert_equal 20, kayak.price_per_hr
-  end 
+  end
+
+  def test_it_has_zero_hours_rented_by_default
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 0, kayak.hours_rented 
+  end
 
 end
