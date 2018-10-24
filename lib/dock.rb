2bd450a05dcc35bc305ff74c72b8fb41dc3a523d
Asa Holt <holt.ace@icloud.com>
2018-10-24 10:32:32 -0600

    super close to the return value i want

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,14 +1,15 @@
+require 'pry'
 class Dock
   attr_reader :name, :max_rental_time, :rental_time
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
-    @revenue_multiplier
+    @revenue_multiplier = []
     @rental_time
   end
 
   def rent(boat, renter)
-    @revenue_multiplier = boat.price_per_hour
+    @revenue_multiplier << boat.price_per_hour
     @rental_time = 0
   end
 
@@ -21,6 +22,9 @@ class Dock
   end
 
   def revenue
-    @rental_time * @revenue_multiplier
+    revenue_array = @revenue_multiplier.map do |price_per_hour|
+      price_per_hour * @rental_time
+    end
+    revenue_array.sum
   end
 end
