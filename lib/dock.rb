901dcd5b2d18b7f8cbb82bf4de7df210a4f3f3f3
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-24 09:55:30 -0600

    Adds initial rent method

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,9 +1,13 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :boats, :revenue
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @boats = []
     @revenue = 0
+  end
+
+  def rent(boat, renter)
+    @boats << boat
   end
 end
