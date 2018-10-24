38fa5b38422a1be8cc4c2efbec4f7c2f32240cc5
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-24 09:11:34 -0600

    adds ability to check hours rented and ensure it defaults to zero

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,9 +1,10 @@
 class Boat
-  attr_reader :type, :price_per_hour
+  attr_reader :type, :price_per_hour, :hours_rented
 
-  def initialize(type, price_per_hour)
+  def initialize(type, price_per_hour, hours_rented = 0)
     @type = type
     @price_per_hour = price_per_hour
+    @hours_rented = hours_rented
   end
 
 end
