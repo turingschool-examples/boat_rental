c2e59911c4204160ae891636588d32bb1b1e1597
Nico <nicovigil1@gmail.com>
2018-10-24 10:08:02 -0600

    adds ability for the dock to test whether it can rent, log hours, return boats, and check how much revenue it's made

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -20,5 +20,46 @@ class DockTest < Minitest::Test
     assert_equal 3, dock.max_rental_time
   end
 
+  def test_dock_can_have_boat_and_boats_can_have_renters
+    dock = Dock.new("The Rowing Dock", 3)
+    ringo = Renter.new("Ringo Star", "4242424242424242")
+    kayak_1 = Boat.new(:kayak, 20)
+    dock.rent(ringo, kayak)
+
+    assert_instance_of Renter, dock.renters
+    assert_instance_of Boat, dock.boats[:kayak_1]
+  end
+
+  def test_dock_can_log_hours
+    dock = Dock.new("The Rowing Dock", 3)
+    ringo = Renter.new("Ringo Star", "4242424242424242")
+    kayak_1 = Boat.new(:kayak, 20)
+    dock.rent(ringo, kayak)
+    dock.log_hour
+
+    assert_equal 1, dock.boats[:kayak_1].hours_rented
+  end
+
+  def test_dock_can_return_boats
+    dock = Dock.new("The Rowing Dock", 3)
+    ringo = Renter.new("Ringo Star", "4242424242424242")
+    kayak_1 = Boat.new(:kayak, 20)
+    dock.rent(ringo, kayak)
+
+    assert_nil boats.find(:kayak_1)
+  end
+
+  def test_dock_can_count_revenue
+    dock = Dock.new("The Rowing Dock", 3)
+    ringo = Renter.new("Ringo Star", "4242424242424242")
+    kayak_1 = Boat.new(:kayak, 20)
+    dock.rent(ringo, kayak)
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+
+    assert_equal 60, dock.revenue 
+  end
 
 end
