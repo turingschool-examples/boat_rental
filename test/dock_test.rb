e0935b19ff28388c4bba86a09742e13e3968d8dc
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:39:09 -0600

    adds first iteration 3 test - revenue

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
+require './lib/boat'
+require './lib/renter'
 require 'pry'
 
 class DockTest < Minitest::Test
@@ -17,6 +19,14 @@ class DockTest < Minitest::Test
   def test_it_has_max_rental_time
     dock = Dock.new("The Rowing Dock", 3)
     assert_equal 3, dock.max_rental_time
+  end
+
+  def test_it_can_get_revenue_for_renting_boat
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak = Boat.new(:kayak, 20)
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    dock.rent_boat(renter, kayak, 1)
+    assert_equal 20, dock.revenue 
   end
 
 end
