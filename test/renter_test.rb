62103da9672f8e96a39957c984715c8066e371af
Tony Robertson <tonyr729@gmail.com>
2018-10-24 09:24:09 -0600

    Add tests for renter properties

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -10,4 +10,12 @@ class MerchantTest < Minitest::Test
   def test_it_exists
     assert_instance_of Renter, @renter
   end
+
+  def test_it_has_a_name
+    assert_equal "Patrick Star", @renter.name
+  end
+
+  def test_it_has_a_cc_number
+    assert_equal "4242424242424242", @renter.credit_card_number
+  end
 end
