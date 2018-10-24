79a1f833c4ba510f544159180a0a2192812f27ae
Ben Lee <bendelonlee@gmail.com>
2018-10-24 09:12:06 -0600

    test_hours_rented_starts_at_0 passes

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,7 +1,8 @@
 class Boat
-  attr_reader :type, :price_per_hour
+  attr_reader :type, :price_per_hour, :hours_rented
   def initialize(type, price_per_hour)
     @type = type
     @price_per_hour = price_per_hour
+    @hours_rented = 0
   end
 end
