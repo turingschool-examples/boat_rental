b6a0e33381a7a491bd7e927fe6f8c09a9e79b08b
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:48:21 -0600

    revises first iteration 3 test. will need to create other tests first before running this one

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,8 +1,17 @@
+require 'time'
+
 class Dock
   attr_reader :name,
               :max_rental_time
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+
+  end
+
+  def rent(boat, renter)
+    boat.start_time = 0
+
+
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -23,10 +23,22 @@ class DockTest < Minitest::Test
 
   def test_it_can_get_revenue_for_renting_boat
     dock = Dock.new("The Rowing Dock", 3)
-    kayak = Boat.new(:kayak, 20)
-    renter = Renter.new("Patrick Star", "4242424242424242")
-    dock.rent_boat(renter, kayak, 1)
-    assert_equal 20, dock.revenue 
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
+    dock.rent(canoe, patrick)
+    dock.log_hour
+    dock.return(kayak_1, patrick)
+    dock.return(kayak_2, patrick)
+    dock.return(canoe, patrick)
+    assert_equal 105, dock.revenue
   end
 
 end
