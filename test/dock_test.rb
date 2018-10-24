1cad8c4d10ea1b7493afaad7c7bee0ec612ac886
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:32:14 -0600

    adds third test

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -11,7 +11,12 @@ class DockTest < Minitest::Test
 
   def test_it_has_a_name
     dock = Dock.new("The Rowing Dock", 3)
-    assert_equal "The Rowing Dock", dock.name 
+    assert_equal "The Rowing Dock", dock.name
+  end
+
+  def test_it_has_max_rental_time
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_equal 3, dock.max_rental_time
   end
 
 end
