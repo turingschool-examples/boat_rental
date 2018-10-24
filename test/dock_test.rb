5d7b6ef90442c38618af8c15d53058210b9a5b24
Ben Lee <bendelonlee@gmail.com>
2018-10-24 09:28:32 -0600

    Adds dock init tests

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -4,9 +4,15 @@ require './lib/dock'
 
 class DockTest < Minitest::Test
   def setup
-    @dock = Dock.new()
+    @dock = Dock.new("The Rowing Dock", 3)
   end
   def test_it_exists
     assert_instance_of Dock, @dock
+  end
+  def test_it_can_be_initialized_with_name
+    assert_equal "The Rowing Dock", @dock.name
+  end
+  def test_it_can_be_initialized_with_max_rental_time
+    assert_equal "The Rowing Dock", @dock.max_rental_time
   end
 end
