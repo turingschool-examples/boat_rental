84576e22b0d3e98036ed4e3dfd5002223846baf8
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-24 09:10:42 -0600

    Adds tests for it_exists and instance variables

---
+++ test/boat_test.rb
@@ -0,0 +1,25 @@
+require './lib/boat'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class BoatTest < Minitest::Test
+  def setup
+    @boat = Boat.new(:kayak, 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Boat, @boat
+  end
+
+  def test_it_has_a_type
+    assert_equal :kayak, @boat.type
+  end
+
+  def test_it_has_a_price_per_hour
+    assert_equal 20, @boat.price_per_hour
+  end
+
+  def test_it_start_with_zero_for_hours_rented
+    assert_equal 0, @boat.hours_rented
+  end
+end
