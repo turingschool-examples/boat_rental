2a211695604f587c9ccc8846a6db9a8972bc4bcb
Mary Goodhart <marya.grace5@gmail.com>
2018-10-24 09:21:35 -0600

    Renter passing tests and class

---
+++ test/renter_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def test_that_it_exists
+  renter = Renter.new("Patrick Star", "4242424242424242")
+
+  assert_instance_of Renter, renter
+  end
+
+  def test_that_it_has_a_name
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    assert_equal "Patrick Star", renter.name
+  end
+
+  def test_that_it_has_a_credit_card_number
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    assert_equal "4242424242424242", renter.credit_card_number
+  end
+  
+end
