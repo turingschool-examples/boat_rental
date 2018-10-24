4cfc4716e1689f06bd42517224ea15ad51905a58
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:09:48 -0600

    adds second test

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -9,4 +9,9 @@ class BoatTest < Minitest::Test
     assert_instance_of Boat, kayak
   end
 
+  def test_it_has_a_type
+    kayak = Boat.new(:kayak, 20)
+    assert_equal :kayak, kayak.type 
+  end
+
 end
