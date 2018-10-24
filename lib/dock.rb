53fdeaad377a5fe3636995aa0625012ee569e250
vjp888 <rellik888@gmail.com>
2018-10-24 09:39:30 -0600

    added rented boats array and rent command to dock. tests work.

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,7 +1,13 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :revenue, :rented_boats
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @revenue = 0
+    @rented_boats = []
+  end
+
+  def rent(boat, name)
+    @rented_boats << boat
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/renter'
 require './lib/dock'
+require './lib/boat'
 
 class DockTest < MiniTest::Test
 
@@ -21,5 +22,19 @@ class DockTest < MiniTest::Test
     dock = Dock.new("The Rowing Dock", 3)
 
     assert_equal 3, dock.max_rental_time
+  end
+
+  def test_dock_has_revenue
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_equal 0, dock.revenue
+  end
+
+  def test_dock_knows_what_boats_are_rented
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    assert_equal [kayak_1], dock.rent(kayak_1, patrick)
   end
 end
