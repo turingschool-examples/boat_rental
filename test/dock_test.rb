db092696a6ba886c9ae7877a4f92d2f049193b49
Tony Robertson <tonyr729@gmail.com>
2018-10-24 09:30:17 -0600

    Add tests for dock properties

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -10,4 +10,12 @@ class MerchantTest < Minitest::Test
   def test_it_exists
     assert_instance_of Dock, @dock
   end
+
+  def test_it_has_a_name
+    assert_equal "The Rowing Dock", @dock.name
+  end
+
+  def test_it_has_a_max_rental_time
+    assert_equal 3, @dock.max_rental_time
+  end
 end
