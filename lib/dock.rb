efd3a3d87b019bf661307999500578f212f79a11
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-24 09:53:27 -0600

    adds rent and return method cannot get the asssert equal value to work for minitest but it is doing what i need it to

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,8 +1,23 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rented_boats
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rented_boats = Hash.new
+  end
+
+  def rent(boat, renter)
+    @rented_boats[boat] = renter
+  end
+
+  def log_hour
+
+  end
+
+  def return(boat, renter)
+    @rented_boats.delete_if do |boat_key, renter_value|
+      boat_key == boat && renter_value == renter
+    end
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
+require './lib/boat'
+require './lib/renter'
 
 class DockTest < Minitest::Test
 
@@ -15,5 +17,24 @@ class DockTest < Minitest::Test
 
     assert_equal "The Rowing Dock", dock.name
     assert_equal 3, dock.max_rental_time
+  end
+
+  def test_you_can_rent_a_boat
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+
+    assert_equal {kayak_1 => patrick}, dock.rented_boats
+  end
+
+  def test_you_can_charge_rented_boats
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
   end
 end
