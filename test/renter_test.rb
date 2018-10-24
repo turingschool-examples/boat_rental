c2e9bd02cf9f2f952b0505a82147d1e2ea8f7902
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:26:03 -0600

    adds third test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -11,6 +11,11 @@ class RenterTest < Minitest::Test
 
   def test_it_has_a_name
     renter = Renter.new("Patrick Star", "4242424242424242")
-    assert_equal "Patrick Star", renter.name 
+    assert_equal "Patrick Star", renter.name
+  end
+
+  def test_it_has_a_credit_card_number
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    assert_equal "4242424242424242", renter.credit_card_number
   end
 end
