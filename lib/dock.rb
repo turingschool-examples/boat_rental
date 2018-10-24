0246f7a5f2e649b9e2a5daf40cdafe7c7caa5f82
abenetka <ali.benetka@gmail.com>
2018-10-24 10:33:20 -0600

    Adds revenue method and corresponding test for It3

--- lib/dock.rb
+++ lib/dock.rb
@@ -4,7 +4,43 @@ class Dock
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @revenue = 0
+    @boats = {}
 
   end
 
+  def rent(boat,renter)
+    @boats[boat] = renter
+  end
+
+  def log_hour
+    @boats.each do |boat, renter|
+        boat.add_hour
+      end
+  end
+
+  def return(boat)
+    @revenue
+  end
+
+  def revenue
+    boat_array = @boats.map do |boat, renter|
+       @revenue += boat.hours_rented * boat.price_per_hour
+     end
+     boat_array.last
+  end
+
 end
+
+
+
+
+#max_rental_time = boat can't be charged for longer than this!
+
+#dock charges the boat's price per hour for hours rented
+
+#rent method: begins tracking boat.hours_rented
+
+#return method: stops tracking boat.hours_rented
+
+#log_hour : +1 hour for any boat thats not yet returned
