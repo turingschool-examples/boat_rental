c4205c01a6d72cf964812c968b1d6f32bd3ee5a2
Maddie Jones <maddyg91@gmail.com>
2018-10-24 09:50:47 -0600

    dock tests passing

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,3 +1,18 @@
 class Dock
-  
+  attr_reader :name, :max_rental_time
+
+  def initialize(name, max_rental_time)
+    @name = name
+    @max_rental_time = max_rental_time
+  end
 end
+
+# pry(main)> dock = Dock.new("The Rowing Dock", 3)
+# #=> #<Dock:0x00007fb5efb36148...>
+#
+# pry(main)> dock.name
+# #=> "The Rowing Dock"
+#
+# pry(main)> dock.max_rental_time
+# #=> 3
+# ```
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -3,5 +3,18 @@ require "minitest/pride"
 require "./lib/dock"
 
 class DockTest < Minitest::Test
+  def test_it_exists
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_instance_of Dock, dock
+  end
 
+  def test_it_has_name
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_equal "The Rowing Dock", dock.name
+  end
+
+  def test_it_has_max
+    dock = Dock.new("The Rowing Dock", 3)
+    assert_equal 3, dock.max_rental_time
+  end
 end
