56731723e6d682bbef4efe5d3e0484533ebab68e
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-24 10:28:54 -0600

    update to revenue method, need to possibly refactor

--- lib/dock.rb
+++ lib/dock.rb
@@ -5,5 +5,39 @@ class Dock
   def initialize(name, max_time)
     @name            = name
     @max_rental_time = max_time
+    @active_rentals  = []
+    @active_renters  = []
+    @return_boat     = []
   end
+
+  def rent(boat, renter)
+    boat.rent_now
+    @active_rentals << boat
+    @active_renters << renter
+  end
+
+  def log_hour
+    @active_rentals.each do |rental|
+      rental.add_hour if rental.rented?
+    end
+  end
+
+  def return(boat)
+    if boat.hours_rented >= @max_rental_time
+      @return_boat << boat
+    end
+  end
+
+  def revenue
+    total = @active_rentals.map do |rental|
+      if rental.hours_rented > @max_rental_time
+        rental.price_per_hour * @max_rental_time
+      else
+        rental.price_per_hour * rental.hours_rented
+      end
+    end
+    total.sum
+    # require 'pry';binding.pry
+  end
+
 end
