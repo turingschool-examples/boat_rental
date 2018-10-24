38b0620601d8b023fe89c184d43b8187a4531ff7
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-24 09:25:27 -0600

    iteration2 completed

---
+++ lib/dock.rb
@@ -0,0 +1,9 @@
+class Dock
+  attr_reader     :name,
+                  :max_rental_time
+
+  def initialize(name, max_time)
+    @name            = name
+    @max_rental_time = max_time
+  end
+end
