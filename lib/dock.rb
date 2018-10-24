5c3df4b6fd06c15ac84e883bc3bd26f2cc9a16bc
Tony Robertson <tonyr729@gmail.com>
2018-10-24 09:41:21 -0600

    Add dock rent method and current_rentals property

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,8 +1,13 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :current_rentals
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @current_rentals = []
+  end
+
+  def rent(boat, renter)
+    @current_rentals << {boat: boat, renter: renter}
   end
 end
