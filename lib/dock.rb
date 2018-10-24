e734517fb1f46d4d86a4bd062d21a4f81e2c06ae
William Petty <william@snakeweed.com>
2018-10-24 09:55:32 -0600

    Adds rented boats array to Dock

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,9 +1,10 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rented_boats
 
   def initialize(name, max_rental_time )
     @name = name
     @max_rental_time = max_rental_time
+    @rented_boats = []
   end
 
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -2,10 +2,19 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 require './lib/dock'
+require './lib/renter'
+require './lib/boat'
 
 class DockTest < Minitest::Test
   def setup
     @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @canoe = Boat.new(:canoe, 25)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   def test_it_exists
@@ -13,13 +22,15 @@ class DockTest < Minitest::Test
   end
 
   def test_it_has_name
-    skip
     assert_equal "The Rowing Dock", @dock.name
   end
 
   def test_it_has_max_rental_time
-    skip
     assert_equal 3, @dock.max_rental_time
+  end
+
+  def test_it_has_rented_boats
+    assert_equal [], @dock.rented_boats
   end
 
 end
