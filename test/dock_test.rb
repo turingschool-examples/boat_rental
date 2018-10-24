779c6c24edfc765c2a5f4ed66f0812e03dac8ed0
Tony Robertson <tonyr729@gmail.com>
2018-10-24 09:41:08 -0600

    Add tests for Dock rent method

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/dock'
+require_relative '../lib/boat.rb'
+require_relative '../lib/renter.rb'
 
 class MerchantTest < Minitest::Test
   def setup
@@ -17,5 +19,17 @@ class MerchantTest < Minitest::Test
 
   def test_it_has_a_max_rental_time
     assert_equal 3, @dock.max_rental_time
+  end
+
+  def test_it_has_no_current_rentals
+    assert_equal [], @dock.current_rentals
+  end
+
+  def test_it_can_rent_boats_to_renters
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    @dock.rent(kayak_1, patrick)
+
+    assert_equal [{:boat=>kayak_1, :renter=>patrick}], @dock.current_rentals
   end
 end
