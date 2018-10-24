6d58950419b9cfc4e570e3d173b429774fa93bba
Tony Robertson <tonyr729@gmail.com>
2018-10-24 10:55:23 -0600

    Add method to track revenue

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,10 +1,11 @@
 class Dock
-  attr_reader :name, :max_rental_time, :current_rentals
+  attr_reader :name, :max_rental_time, :current_rentals, :rental_history
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @current_rentals = []
+    @rental_history = []
   end
 
   def rent(boat, renter)
@@ -17,10 +18,18 @@ class Dock
       boat.add_hour if boat.hours_rented < @max_rental_time
     end
   end
-
+  
   def return(boat)
-    @current_rentals.delete_if do |rental|
-      rental[:boat] == boat
+    @current_rentals.each do |rental|
+      @rental_history << rental if rental[:boat] == boat
+    end
+    @current_rentals.delete(@rental_history.last)
+  end
+  
+  def revenue
+    @rental_history.reduce(0) do |revenue, rental|
+      revenue += rental[:boat].price_per_hour * rental[:boat].hours_rented
+      revenue
     end
   end
 end
