0e57c232036dfdac649047d7218d817ba41b251a
Aizar <aizar.cabrera@icloud.com>
2018-10-24 09:18:18 -0600

    adds test for type

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,5 +1,8 @@
 class Boat
 
+  attr_reader :type
+
   def initialize(type, price)
+    @type = type
   end
 end
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -4,8 +4,17 @@ require 'minitest/pride'
 require 'pry'
 
 class BoatTest < Minitest::Test
+
   def test_it_exists
     kayak = Boat.new(:kayak, 20)
     assert_instance_of Boat, kayak
   end
+
+  def test_it_exists
+    kayak = Boat.new(:kayak, 20)
+    assert_equal :kayak, kayak.type
+  end
+
+
+
 end
