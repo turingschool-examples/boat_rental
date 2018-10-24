3ebe3c44a5aa155712d6d0fb6c02c77110950737
Leah Miller <lkmiller.87@gmail.com>
2018-10-24 09:30:44 -0600

    Add passing test for adding boats to inventory

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,8 +1,14 @@
 class Dock
 
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :boat_inventory
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @boat_inventory = []
   end
+
+  def add_boats_to_inventory(boat)
+    @boat_inventory << boat
+  end
+
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -2,7 +2,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/boat'
 require './lib/dock'
-# require './lib/renter'
+require './lib/renter'
+
 class DockTest < Minitest::Test
 
   def test_it_exists
@@ -18,6 +19,21 @@ class DockTest < Minitest::Test
   def test_it_has_max_rental_time
     dock = Dock.new("The Rowing Dock", 3)
     assert_equal 3, dock.max_rental_time
+  end
+
+  def test_it_has_a_boat_inventory
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    dock.add_boats_to_inventory(kayak_1)
+    dock.add_boats_to_inventory(kayak_2)
+    dock.add_boats_to_inventory(canoe)
+    dock.add_boats_to_inventory(sup_1)
+    dock.add_boats_to_inventory(sup_2)
+    assert_equal [kayak_1, kayak_2, canoe, sup_1, sup_2], dock.boat_inventory
   end
 
 end
