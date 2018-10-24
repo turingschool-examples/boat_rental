e8a89a6e2818b3feab9b3e7c4d36dcf0d02126a6
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-24 09:11:52 -0600

    Add renter tests

---
+++ test/renter_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+  
+  def test_it_exsists
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    assert_instance_of Renter, renter
+  end
+  
+  def test_it_has_a_name
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    assert_equal "Patrick Star", renter.name
+  end
+  
+  def test_it_has_a_credit_card_number
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    assert_equal "4242424242424242", renter.credit_card_number
+  end
+  
+end
