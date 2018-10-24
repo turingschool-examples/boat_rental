c598e7b26929e025377e17b346e592b3dce99b60
Maddie Jones <maddyg91@gmail.com>
2018-10-24 09:06:39 -0600

    added test is exists for boat

---
+++ lib/boat.rb
@@ -0,0 +1,30 @@
+
+
+#
+# pry(main)> require './lib/boat'
+# #=> true
+#
+# pry(main)> kayak = Boat.new(:kayak, 20)
+# #=> #<Boat:0x00007fceac8f0480...>
+#
+# pry(main)> kayak.type
+# #=> :kayak
+#
+# pry(main)> kayak.price_per_hour
+# #=> 20
+#
+# pry(main)> kayak.hours_rented
+# #=> 0
+#
+# pry(main)> kayak.add_hour
+# #=> 1
+#
+# pry(main)> kayak.add_hour
+# #=> 2
+#
+# pry(main)> kayak.add_hour
+# #=> 3
+#
+# pry(main)> kayak.hours_rented
+# #=> 3
+# ```
