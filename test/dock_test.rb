a07a4b449932adfcb4100321dd408fda53d36fef
Ben Lee <bendelonlee@gmail.com>
2018-10-24 09:34:18 -0600

    Wrote long setup for DockTest

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,9 +1,18 @@
 require 'minitest/autorun'
 require './lib/dock'
+require './lib/boat'
+require './lib/renter'
 
 
 class DockTest < Minitest::Test
   def setup
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @canoe = Boat.new(:canoe, 25)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
     @dock = Dock.new("The Rowing Dock", 3)
   end
   def test_it_exists
