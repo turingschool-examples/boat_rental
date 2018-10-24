1593e70016130f358c788f858db31c91f33f87ea
Daniel Briechle <danbriechle@hotmail.com>
2018-10-24 10:28:18 -0600

    created log hour method and corresponding test

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,7 +1,31 @@
+
+require 'pry'
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rented_boat, :renters
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rented_boat = []
+    @renters = []
+
   end
+
+
+
+  def rent(boat, renter)
+  @rented_boat << boat
+  @renters << renter
+  end
+
+
+  def log_hour
+    @rented_boat.each do |boat|
+     boat.add_hour
+   end
+    total_hours = @rented_boat.map do |boat|
+     boat.hours_rented
+    end
+     total_hours.sum
+  end
+
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require "minitest/pride"
 require './lib/dock'
+require './lib/renter'
+require './lib/boat'
 
 class DockTest < MiniTest::Test
   def test_it_exists
@@ -13,5 +15,31 @@ class DockTest < MiniTest::Test
     assert_equal "The Rowing Dock", dock.name
     assert_equal 3, dock.max_rental_time
   end
+
+  def test_boats_can_be_rented
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    dock.rent(kayak_1, patrick)
+    assert_equal [kayak_1], dock.rented_boat
+    assert_equal [patrick], dock.renters
+  end
+
+  def test_hours_can_be_loged
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
+    assert_equal 4, dock.log_hour
+  end
+
+  
+
+
+
+
 
 end
