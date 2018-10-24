2253c276b7e9b9e355826eb75bde6bde8b581565
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-24 09:15:36 -0600

    Iteration 0 complete. File setup for Iteration 2 started

---
+++ test/boat_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/boat'
+
+class BoatTest < Minitest::Test
+
+  def test_it_exists
+    kayak = Boat.new(:kayak, 20)
+    assert_instance_of Boat, kayak
+  end
+
+  def test_it_has_a_type
+    kayak = Boat.new(:kayak, 20)
+    assert_equal :kayak, kayak.type
+  end
+
+  def test_it_has_price_per_hour
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 20, kayak.price_per_hour
+  end
+
+  def test_it_has_hours_rented
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_it_can_add_hours
+    kayak = Boat.new(:kayak, 20)
+    assert_equal 1, kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+    assert_equal 3, kayak.hours_rented
+  end
+
+
+end
---
+++ test/dock_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dock'
+
+class DockTest < Minitest::Test
+
+
+end
