6bc1693c6ea2e475573a44825f32fa74d5136f21
Brad Niedt <bradleyniedt@gmail.com>
2018-10-24 09:24:15 -0600

    adds second test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -8,4 +8,9 @@ class RenterTest < Minitest::Test
     renter = Renter.new("Patrick Star", "4242424242424242")
     assert_instance_of Renter, renter
   end
+
+  def test_it_has_a_name
+    renter = Renter.new("Patrick Star", "4242424242424242")
+    assert_equal "Patrick Star", renter.name 
+  end
 end
