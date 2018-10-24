3f630ecef6269fbe75d4aca98d7ad6f086ae458d
jpclark6 <jpclark6@gmail.com>
2018-10-24 09:32:06 -0600

    Adds remaining dock tests

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -3,8 +3,19 @@ require 'minitest/pride'
 require 'pry'
 require './lib/dock'
 require './lib/renter'
+require './lib/boat'
 
 class DockTest < Minitest::Test
+  def setup
+    @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @canoe = Boat.new(:canoe, 25)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
+  end
   def test_it_exists
     dock = Dock.new("The Rowing Dock", 3)
     assert_instance_of Dock, dock
@@ -19,4 +30,51 @@ class DockTest < Minitest::Test
     dock = Dock.new("The Rowing Dock", 3)
     assert_equal 3, dock.max_rental_time
   end
+
+  def test_it_can_rent_boats
+    @dock.rent(@kayak_1, @patrick)
+    assert_equal [[@kayak_1, @patrick]], @dock.rented_boats
+  end
+
+  def test_it_can_log_an_hour
+    @dock.rent(@kayak_1, @patrick)
+    assert_equal 0, @dock.hours_logged_by_boat(@kayak_1)
+    @dock.log_hour
+    assert_equal 1, @dock.hours_logged_by_boat(@kayak_1)
+  end
+
+  def test_it_cant_be_rented_longer_than_max_time
+    @dock.rent(@kayak_1, @patrick)
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    assert_equal 3, @dock.hours_logged_by_boat(@kayak_1)
+    @dock.log_hour
+    assert_equal 3, @dock.hours_logged_by_boat(@kayak_1)
+  end
+
+  def test_it_can_return_a_boat
+    assert_equal false, @dock.rented?(@kayak_1)
+    @dock.rent(@kayak_1, @patrick)
+    assert_equal true, @dock.rented?(@kayak_1)
+  end
+
+  def test_it_can_calculate_revenue
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    @dock.return(@kayak_1)
+    @dock.return(@kayak_2)
+    @dock.return(@canoe)
+    assert_equal 105, @dock.revenue
+    @dock.rent(@sup_1, @eugene)
+    @dock.rent(@sup_2, @eugene)
+    5.times { dock.log_hour }
+    @dock.return(@sup_1)
+    @dock.return(@sup_2)
+    assert_equal 195, @dock.revenue
+  end
+
 end
