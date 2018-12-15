139fb56b32a92b58921d8d6d3d48bae2a01d2cba
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-15 14:30:25 -0700

    Added attributes to boat

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,6 +1,13 @@
 class Boat
 
+  attr_reader :type,
+              :price_per_hour,
+              :hours_rented
+
   def initialize(type, price_per_hour)
+    @type = type
+    @price_per_hour = price_per_hour
+    @hours_rented = 0
   end
 
 end
--- test/boat_test.rb
+++ test/boat_test.rb
@@ -12,4 +12,13 @@ class BoatTest < Minitest::Test
     assert_instance_of Boat, @kayak
   end
 
+  def test_it_has_attributes
+    assert_equal :kayak, @kayak.type
+    assert_equal 20, @kayak.price_per_hour
+  end
+
+  def test_it_starts_with_no_hours_rented
+    assert_equal 0, @kayak.hours_rented
+  end
+
 end
