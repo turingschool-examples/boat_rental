b6d56f61719bf41a6ba1326d7cadeaed5486237a
Matt Levy <milevy1@gmail.com>
2019-01-27 14:33:40 -0700

    Added renter rests for iteration 1

--- tests/renter_test.rb
+++ tests/renter_test.rb
@@ -4,4 +4,16 @@ require './renter/card'
 
 class RenterTest < Minitest::Test
 
+  def setup
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  def test_renter_has_a_name
+    assert_equal "Patrick Star", @renter.name
+  end
+
+  def test_renter_has_a_credit_card_number
+    assert_equal "4242424242424242", @renter.credit_card_number
+  end
+
 end
