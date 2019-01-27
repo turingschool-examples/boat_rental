bd4328a82eda4015ba9d2bdaa2475df5a8c89a5b
Matt Levy <milevy1@gmail.com>
2019-01-27 14:32:54 -0700

    Added boat tests for iteration 1

--- tests/boat_test.rb
+++ tests/boat_test.rb
@@ -12,6 +12,16 @@ class BoatTest < Minitest::Test
     assert_equal :kayak, @kayak.type
   end
 
-  
+  def test_boat_has_price_per_hour
+    assert_equal 20, @kayak.price_per_hour
+  end
+
+  def test_hours_rented_works
+    assert_equal 0, @kayak.hours_rented
+    @kayak.add_hour
+    @kayak.add_hour
+    @kayak.add_hour
+    assert_equal 3, @kayak.hours_rented
+  end
 
 end
