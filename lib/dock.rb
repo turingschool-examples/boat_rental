c425071f0b9c5f5426857d24f992b35bec4fa54a
Vince Carollo <vincecarollo@gmail.com>
2019-03-30 17:07:34 -0600

    Finish iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -10,4 +10,16 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    charge_report = {}
+    renter = @rental_log[boat]
+    if boat.hours_rented > @max_rental_time
+      boat.hours_rented = @max_rental_time
+    end
+    charged_amount = boat.price_per_hour * boat.hours_rented
+    charge_report[:card_number] = renter.credit_card_number
+    charge_report[:amount] = charged_amount
+    charge_report
+  end
 end
