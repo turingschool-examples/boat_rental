c6ae925327fe7cf99cc28108d69236586e1c2d63
davisc0801 <davisc1014@gmail.com>
2019-03-30 05:50:58 -0600

    Add boat class tests

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -2,4 +2,32 @@ require "./lib/boat"
 
 describe Boat do
 
+  before do
+    @kayak = Boat.new(:kayak, 20)
+  end
+
+  it "has a type" do
+    expect(@kayak.type).to eq(:kayak)
+  end
+
+  it "has a price per hour"
+    expect(@kayak.price_per_hour).to eq(20)
+  end
+
+  it "starts with zero hours rented" do
+    expect(@kayak.hours_rented).to eq(0)
+  end
+
+  context "add hours" do
+
+    before do
+      @kayak.add_hour
+      @kayak.add_hour
+      @kayak.add_hour
+    end
+
+    it "tracks rented hours" do
+      expect(@kayak.hours_rented).to eq(3)
+    end
+  end
 end
