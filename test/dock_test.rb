e279b0313344eb40934be7c4e84b20a08d42b505
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-02 22:04:22 -0600

    Add rent method and rental_log attr to Dock

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,10 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
+require './lib/renter'
+require './lib/boat'
 
 class DockTest < Minitest::Test
   def setup
     @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   def test_it_exists
@@ -14,5 +21,19 @@ class DockTest < Minitest::Test
   def test_it_inits_with_name_and_max_rental_time
     assert_equal "The Rowing Dock", @dock.name
     assert_equal 3, @dock.max_rental_time
+  end
+
+  def test_rent_adds_to_rental_log_hash
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.rent(@sup, @eugene)
+
+    expected = {
+      @kayak_1 => @patrick,
+      @kayak_2 => @patrick,
+      @sup => @eugene
+    }
+
+    assert_equal expected, @dock.rental_log
   end
 end
