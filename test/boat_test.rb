a9633d53904496637b4dc13b786ccdb1c443352b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-02 21:47:25 -0600

    Add init of Boat

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -3,4 +3,16 @@ require 'minitest/pride'
 require './lib/boat'
 
 class BoatTest < Minitest::Test
+  def setup
+    @kayak = Boat.new(:kayak, 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Boat, @kayak
+  end
+
+  def test_it_inits_with_type_and_price_per_hour
+    assert_equal :kayak, @kayak.type
+    assert_equal 20, @kayak.price_per_hour
+  end
 end
