62d10672d52bf4ca2dd34508ea6d106a62423d3b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-02 21:57:27 -0600

    Add skeleton/init of Dock

---
+++ test/dock_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dock'
+
+class DockTest < Minitest::Test
+  def setup
+    @dock = Dock.new("The Rowing Dock", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Dock, @dock
+  end
+
+  def test_it_inits_with_name_and_max_rental_time
+    assert_equal "The Rowing Dock", @dock.name
+    assert_equal 3, @dock.max_rental_time
+  end
+end
