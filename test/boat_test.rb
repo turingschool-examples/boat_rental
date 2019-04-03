722d78f5c4b4b53c02cf740371413d5bab355361
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-02 21:45:31 -0600

    Add Boat hours methods tests

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -14,4 +14,15 @@ class BoatTest < Minitest::Test
     assert_equal :kayak, @kayak.type
     assert_equal 20, @kayak.price_per_hour
   end
+
+  def test_it_starts_with_zero_hours_rented
+    assert_equal 0, @kayak.hours_rented
+  end
+
+  def test_it_can_add_hours_and_update
+    @kayak.add_hour
+    @kayak.add_hour
+
+    assert_equal 2, @kayak.hours_rented
+  end
 end
