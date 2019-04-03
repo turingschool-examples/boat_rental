c63263628322fa12d5a823cc2b759e0d8905c0ed
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-02 22:45:22 -0600

    Test full sequence of Dock

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -7,7 +7,9 @@ class DockTest < Minitest::Test
     @dock = Dock.new("The Rowing Dock", 3)
     @kayak_1 = Boat.new(:kayak, 20)
     @kayak_2 = Boat.new(:kayak, 20)
+    @canoe = Boat.new(:canoe, 25)
     @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @sup_2 = Boat.new(:standup_paddle_board, 15)
     @patrick = Renter.new("Patrick Star", "4242424242424242")
     @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
@@ -88,5 +90,33 @@ class DockTest < Minitest::Test
 
     assert_equal ({}), @dock.rental_log
     assert_equal 20, @dock.revenue
+  end
+
+  def test_full_sequence
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.log_hour
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+
+    assert_equal 0, @dock.revenue
+
+    @dock.return(@kayak_1)
+    @dock.return(@kayak_2)
+    @dock.return(@canoe)
+
+    assert_equal 105, @dock.revenue
+
+    @dock.rent(@sup_1, @eugene)
+    @dock.rent(@sup_2, @eugene)
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.log_hour
+    @dock.return(@sup_1)
+    @dock.return(@sup_2)
+
+    assert_equal 195, @dock.revenue
   end
 end
