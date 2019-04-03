dd6739aed909533ec87d739a40aaffb36e74ce72
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-02 21:52:15 -0600

    Add init of Renter

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,4 +3,11 @@ require 'minitest/pride'
 require './lib/renter'
 
 class RenterTest < Minitest::Test
+  def setup
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  def test_it_exists
+    assert_instance_of Renter, @renter
+  end
 end
