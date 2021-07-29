8b7b8bebf0152a7fb74a72e072e5bdaf0a449167
ChloeLL <chloell@protonmail.ch>
2021-07-28 20:49:49 -0700

    Complete iteration 1

---
+++ spec/renter_spec.rb
@@ -0,0 +1,19 @@
+require './lib/renter'
+
+RSpec.describe Renter do
+  before(:each) do
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  it 'exists' do
+    expect(@renter).to be_a(Renter)
+  end
+
+  it 'gives a name' do
+    expect(@renter.name).to eq("Patrick Star")
+  end
+
+  it 'gives a credit_card_number' do
+    expect(@renter.credit_card_number).to eq("4242424242424242")
+  end
+end
