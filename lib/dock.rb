7f09441f81a7cfc003622d2418f510a64f639ce8
Nick Jacobs <nickjacobsatc@gmail.com>
2022-11-08 14:36:25 -0600

    Feat: charge and rent methods created and tested

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,9 +1,26 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rental_log
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rental_log = {}
+  end
+
+  def rent(boat,renter)
+    @rental_log[boat] = renter
+  end
+
+  def charge(boat)
+    charge_hash = {
+      :credit_card_number => @rental_log[boat].credit_card_number,
+      :amount =>
+      if boat.hours_rented <= max_rental_time
+        boat.hours_rented * boat.price_per_hour
+      else
+        boat.price_per_hour * max_rental_time
+      end
+    }
   end
 
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -10,4 +10,55 @@ RSpec.describe Dock do
     expect(dock.max_rental_time).to eq(3)
   end
 
+  it "can rent a boat to a renter" do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    expect(dock.rental_log.count).to eq(3)
+    expected = {
+      kayak_1 => patrick,
+      kayak_2 => patrick,
+      sup_1 => eugene
+    }
+    expect(dock.rental_log).to eq(expected)
+    # require 'pry';binding.pry
+  end
+
+  it "can charge a renter for a boat" do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    kayak_1.add_hour
+    kayak_1.add_hour
+    expected = {
+      :credit_card_number => "4242424242424242",
+      :amount => 40
+    }
+    expect(dock.charge(kayak_1)).to eq(expected)
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    expected2 = {
+      :credit_card_number => "1313131313131313",
+      :amount => 45
+    }
+    expect(dock.charge(sup_1)).to eq(expected2)
+    require 'pry';binding.pry
+
+  end
+
 end
